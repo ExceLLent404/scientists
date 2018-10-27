008c5f3c473dedc7066b6717f1a338579c52dd75
ExceLLent <zerkc_97@mail.ru>
2018-10-27 11:31:26 +0300

    Add REST API methods to add data

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ require 'sequel'
 require 'json'
 require_relative 'models'
 require_relative 'db_connection'
+require_relative 'checking_methods'
 
 get '/api/scientists' do
   scientists = Scientist.all
@@ -64,4 +65,49 @@ end
 
 get '/api/devices/:d_id/scientists/:s_id' do
   redirect to("/api/scientists/#{params[:d_id]}")
+end
+
+post '/api/scientists' do
+  data = JSON.parse(request.body.read)
+  halt 422 until is_correct_data?(data, "name", "madness", "tries")
+
+  Scientist.create(name: data["name"],
+    madness: data["madness"], tries: data["tries"])
+  return 201
+end
+
+post '/api/scientists/:id/devices' do
+  scientist = Scientist.first(id: params[:id])
+  halt 404 if scientist.nil?
+
+  data = JSON.parse(request.body.read)
+  halt 422 until is_correct_data?(data, "id")
+
+  device = Device.first(id: data["id"])
+  halt 404 if device.nil?
+
+  scientist.add_device(device)
+  return 201
+end
+
+post '/api/devices' do
+  data = JSON.parse(request.body.read)
+  halt 422 until is_correct_data?(data, "name", "power")
+
+  Device.create(name: data["name"], power: data["power"])
+  return 201
+end
+
+post '/api/devices/:id/scientists' do
+  device = Device.first(id: params[:id])
+  halt 404 if device.nil?
+
+  data = JSON.parse(request.body.read)
+  halt 422 until is_correct_data?(data, "id")
+
+  scientist = Scientist.first(id: data["id"])
+  halt 404 if scientist.nil?
+
+  device.add_scientist(scientist)
+  return 201
 end

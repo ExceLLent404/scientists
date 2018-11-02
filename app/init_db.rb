683dd3c9dcd38f157fc11ff261c43d486b797481
Ivan Korolev <zerkc_97@mail.ru>
2018-11-02 12:52:04 +0300

    Use Sequel syntax instead of raw SQL

--- app/init_db.rb
+++ app/init_db.rb
@@ -1,9 +1,30 @@
 require 'sequel'
 require_relative 'db_connection'
 
-file = File.open("app/init_db.sql", "r")
-query = file.read
+DB.drop_table?(:copyrights, :scientists, :devices)
 
-DB.run(query)
+DB.create_table(:scientists) do
+  primary_key :id
+  String :name, size: 40, null: false, unique: true
+  Integer :madness, null: false         # measure of madness
+  Integer :tries, null: false           # number of tries to destroy the galaxy
+  DateTime :timestamp, null: false, default: Time.now # date and time of
+                                                      # adding information
+  constraint(:not_negative_madness) { madness >= 0 }
+  constraint(:not_negative_tries) { tries >= 0 }
+end
 
-file.close
+DB.create_table(:devices) do
+  primary_key :id
+  String :name, size: 40, null: false, unique: true
+  Integer :power, null: false           # measure of destructive power
+  DateTime :timestamp, null: false, default: Time.now # date and time of
+                                                      # adding information
+  constraint(:not_negative_power) { power >= 0 }
+end
+
+DB.create_table(:copyrights) do
+  foreign_key :scientist_id, :scientists, on_delete: :cascade
+  foreign_key :device_id, :devices, on_delete: :cascade
+  primary_key [:scientist_id, :device_id]
+end
